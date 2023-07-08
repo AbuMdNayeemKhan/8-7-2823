261741c3b155be8ceea8c72bce2bd7ee37010064
unknown <abumohammadnayemkhan@gmail.com>
2023-07-09 01:48:55 +0600

    learning javascript

---
+++ app.js
@@ -0,0 +1,35 @@
+//==========Function return start
+// function returnData(data1, data2){
+//     return data1 + data2;
+// }
+// console.log(returnData(12, 3) + " Taka");
+//==========Function return ended
+
+//==========javascript Object Start
+// var myPc = {
+//     processor: "Ryzan 5600G",
+//     monitor: {
+//         primari: "dell",
+//         secondary: "msi",
+//     },
+//     color: "pink",
+//     price: 70000
+// }
+// console.log(myPc['processor']);
+//==========Javascript Object Ended
+
+//==========JavaScript array Start
+var cityOfBD = ["Dhaka", "Barishal", "khulna", "Rajsahi", "Chottogram", "Seleat"];
+//==========JavaScript array ended
+
+//==========for loop in javascript
+// for(let i = 0; i < cityOfBD.length; i++){
+//     console.log(cityOfBD[i]);
+// };
+//==========for loop in javascript
+
+//==========for in loop start
+for(let item in cityOfBD){
+    console.log(cityOfBD[item]);
+}
+//==========for in loop ended
